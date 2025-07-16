c20662f6fbd52b4c1835a82b787d151357ad569d
Jalegosa <jalegosa01@gmail.com>
2025-07-15 23:55:31 -0600

    print

--- main.py
+++ main.py
@@ -1,1 +1,17 @@
-print("Hola")
+inventario = {}
+
+cantidad = int(input("\n¿Cuántos productos desea ingresar?: "))
+
+for i in range(cantidad):
+    print(f"\n Producto #{i + 1}")
+
+    while True:
+        codigo = int(input("Ingrese el código numérico del producto: "))
+        try:
+            int_codigo = int(codigo)
+            if codigo in inventario:
+                print("EsTe código ya existe. Ingrese uno diferente.")
+            else:
+                break
+        except ValueError:
+            print("El código debe contener solo números.")
